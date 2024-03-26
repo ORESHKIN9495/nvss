9980c81d7b68129ac25a2cdc5e279cb171913b3d
Илья Орешкин <ioreshkin@outlook.com>
2024-03-26 23:50:12 +0300

    Компоненты

---
+++ src/compossables/scrollToElements.ts
@@ -0,0 +1,49 @@
+import { links } from "../stores/useLinks";
+
+const header = document.querySelector("header") as HTMLElement;
+
+const lists = links.map(
+  (link) => document.querySelector(`.${link.tab}`) as HTMLElement
+);
+
+let index = 0;
+
+const scrollToElement = (id: string) => {
+  window.scrollTo({
+    top:
+      (document.querySelector(`#${id}`) as HTMLElement)?.offsetTop -
+      header.clientHeight,
+    behavior: "smooth",
+  });
+};
+
+const watch = () => {
+  links.forEach((el, idx) => {
+    if (
+      document.documentElement.scrollTop + header.clientHeight >=
+      (document.querySelector(`#${el.tab}`) as HTMLElement)?.offsetTop
+    ) {
+      index = idx;
+    }
+  });
+
+  if (
+    window.innerHeight + document.documentElement.scrollTop >=
+    document.documentElement.offsetHeight -
+      (document.querySelector("#tab-4") as HTMLElement)?.clientHeight
+  ) {
+    index = 3;
+  }
+
+  lists.forEach((li) => {
+    if (li instanceof HTMLElement) {
+      li.classList.remove("active");
+
+      if (li.dataset.tab === links[index].tab) {
+        li.classList.add("active");
+      }
+    }
+  });
+};
+
+export { scrollToElement, watch };
---
+++ src/stores/useLinks.ts
@@ -0,0 +1,24 @@
+const links = [
+  {
+    id: 1,
+    name: "Главная",
+    tab: "tab-1",
+  },
+  {
+    id: 2,
+    name: "Изоляция стальных труб",
+    tab: "tab-2",
+  },
+  {
+    id: 3,
+    name: "О Компании",
+    tab: "tab-3",
+  },
+  {
+    id: 4,
+    name: "Контакты",
+    tab: "tab-4",
+  },
+];
+
+export { links };
